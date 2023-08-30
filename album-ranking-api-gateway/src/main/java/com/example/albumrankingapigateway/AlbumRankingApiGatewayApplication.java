dba67e57f4cc0233576be477a7a441ee6faee215
Barti <bartosz.majkowski@wp.pl>
2023-08-30 18:19:57 +0200

    project_fixed

---
+++ album-ranking-api-gateway/src/main/java/com/example/albumrankingapigateway/AlbumRankingApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.albumrankingapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumRankingApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumRankingApiGatewayApplication.class, args);
+	}
+
+}
---
+++ album-ranking-api-gateway/src/test/java/com/example/albumrankingapigateway/AlbumRankingApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.albumrankingapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumRankingApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
